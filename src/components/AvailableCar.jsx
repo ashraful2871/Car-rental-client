2a9fe71ca40ea3b0b700ce4bb8100da8b38a3000
ashraful2871 <ashrafulislam2758@gmail.com>
2024-12-24 23:11:20 +0600

    make functionality for toggle between grid and list view

--- src/components/AvailableCar.jsx
+++ src/components/AvailableCar.jsx
@@ -2,11 +2,14 @@ import axios from "axios";
 import React from "react";
 import { useEffect, useState } from "react";
 import AvailableCarCard from "./AvailableCarCard";
+import { Link } from "react-router-dom";
+import ListView from "./ListView";
 
 const AvailableCar = () => {
   const [cars, setCars] = useState([]);
   const [search, setSearch] = useState("");
   const [sort, setSort] = useState("");
+  const [view, setView] = useState("grid");
 
   console.log(search, sort);
 
@@ -22,56 +25,67 @@ const AvailableCar = () => {
 
   return (
     <div className="space-y-9">
-      <div className="flex flex-col md:flex-row justify-center items-center gap-5 ">
-        {/* <div>
-          <select
-            name="category"
-            id="category"
-            className="border p-4 rounded-lg"
-          >
-            <option value="">Filter By Category</option>
-            <option value="Web Development">Web Development</option>
-            <option value="Graphics Design">Graphics Design</option>
-            <option value="Digital Marketing">Digital Marketing</option>
-          </select>
-        </div> */}
+      <div className="flex  justify-between">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-5 ">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
+              <input
+                className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent "
+                type="text"
+                name="search"
+                placeholder="Enter Car Model, Location"
+                aria-label="Enter Car Model, Location"
+                onChange={(e) => setSearch(e.target.value)}
+              />
 
-        <form onSubmit={(e) => e.preventDefault()}>
-          <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
-            <input
-              className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent "
-              type="text"
-              name="search"
-              placeholder="Enter Car Model, Location"
-              aria-label="Enter Car Model, Location"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-
-            <button className="btn btn-primary text-white font-bold text-base">
-              Search
-            </button>
+              <button className="btn btn-primary text-white font-bold text-base">
+                Search
+              </button>
+            </div>
+          </form>
+          <div>
+            <select
+              name="category"
+              id="category"
+              className="border p-4 rounded-md"
+              onChange={(e) => setSort(e.target.value)}
+            >
+              <option value="">Sort By Date, price</option>
+              <option value="date-dsc">Date (Newest First)</option>
+              <option value="price-asc">Price (Lowest First)</option>
+            </select>
           </div>
-        </form>
-        <div>
-          <select
-            name="category"
-            id="category"
-            className="border p-4 rounded-md"
-            onChange={(e) => setSort(e.target.value)}
-          >
-            <option value="">Sort By Date, price</option>
-            <option value="date-dsc">Date (Newest First)</option>
-            <option value="price-asc">Price (Lowest First)</option>
-          </select>
+          <button className="btn btn-primary text-white font-bold text-base">
+            Reset
+          </button>
         </div>
-        <button className="btn btn-primary text-white font-bold text-base">
-          Reset
-        </button>
+        <div className="flex gap-5">
+          <button
+            onClick={() => setView("grid")}
+            className="btn btn-primary text-white font-bold text-base"
+          >
+            Grid View
+          </button>
+          <button
+            onClick={() => setView("list")}
+            className="btn btn-neutral text-white font-bold text-base"
+          >
+            List View
+          </button>
+        </div>
       </div>
-      <div className="grid grid-cols-4 gap-6">
-        {cars.map((car, idx) => (
-          <AvailableCarCard car={car} key={idx}></AvailableCarCard>
-        ))}
+      <div
+        className={`${
+          view === "grid" ? "grid grid-cols-4 gap-6" : "space-y-6"
+        }`}
+      >
+        {cars.map((car, idx) =>
+          view === "grid" ? (
+            <AvailableCarCard car={car} key={idx}></AvailableCarCard>
+          ) : (
+            <ListView car={car} key={idx}></ListView>
+          )
+        )}
       </div>
     </div>
   );
---
+++ src/components/ListView.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ListView = ({ car }) => {
+  const {
+    _id,
+    photo,
+    model,
+    rentalPrice,
+    date,
+    availability,
+    location,
+    booking_count,
+  } = car;
+  return (
+    <div className="card card-side bg-base-100 border-2 p-4 gap-5">
+      <div className="w-[750px] ">
+        <img
+          className=" w-full h-full rounded-xl"
+          src="https://i.ibb.co.com/WB2wdgg/bmw.jpg"
+          alt="Movie"
+        />
+      </div>
+      <div className="w-full font-semibold space-y-3 ">
+        <h2 className="card-title text-3xl">{model}</h2>
+        <p>Posted Date: {date}</p>
+        <p>Price Per Day: ${rentalPrice}</p>
+        <p>Availability: {availability}</p>
+        <p>Location: {location}</p>
+        <p>Total Booking: {booking_count}</p>
+        <div className="card-actions justify-end">
+          <Link to={`/car_details/${_id}`}>
+            <button className="btn btn-primary text-white font-bold text-base">
+              Book Now
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListView;
