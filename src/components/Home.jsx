6874a1b08928ecebd94a8685b99cb8af21dcc827
ashraful2871 <ashrafulislam2758@gmail.com>
2024-12-24 23:52:37 +0600

    add why choose us section in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "./banner/Banner";
+import WhyChooseUs from "./WhyChooseUs";
 
 const Home = () => {
   return (
     <div>
       <h2>this is home</h2>
       <Banner></Banner>
+      <WhyChooseUs></WhyChooseUs>
     </div>
   );
 };
