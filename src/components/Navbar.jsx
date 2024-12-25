2fa0b969881c838d7d0c1d30d0c001f8f3ff576f
ashraful2871 <ashrafulislam2758@gmail.com>
2024-12-25 23:22:28 +0600

    delete code cart icon and dropdown in navbar from daisiUI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,20 +92,6 @@ const Navbar = () => {
         <div>
           <DarkLightMood></DarkLightMood>
         </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
-        </div>
         {user && (
           <div className="dropdown dropdown-end z-50">
             <div
